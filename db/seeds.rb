296969ffd74bd3721195636cbfc706b8ab3bc518
Jules Rivoire <julesriv@gmail.com>
2019-02-13 16:13:22 +0100

    kinda finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+ Restaurant.create(name: 'Fulgurances', address: '10 rue alexeandre dumas', phone_number: '01 45 65 74 84', category: 'french')
+ Restaurant.create(name: "tour d'argent", address: 'Paris', phone_number: '01 54 65 74 84', category: 'italian')
+ Restaurant.create(name: 'Brigade', address: 'Londres', phone_number: '01 76 65 74 84', category: 'chinese')
+ Restaurant.create(name: "T'aime", address: 'Santiago', phone_number: '01 28 65 74 84', category: 'japanese')
+ Restaurant.create(name: 'Domac', address: 'Buenos aires', phone_number: '01 47 65 74 84', category: 'belgian')
